75c6155ed148e37646854d49449431d512a77ec4
Josue Costa <juncosta@gmail.com>
2024-03-02 23:31:54 -0300

    initial commit

---
+++ db/migrate/20240303021543_create_hairdressers.rb
@@ -0,0 +1,12 @@
+class CreateHairdressers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :hairdressers do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
