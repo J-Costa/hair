e9fd38dff9df77f0a23a4e8e9735fc9230c8d11d
Josue Costa <juncosta@gmail.com>
2024-03-04 17:25:14 -0300

    Add alert component and update customer form layout

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,19 @@
 class CustomersController < ApplicationController
-  def new
+
+  def new; end
+
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      redirect_to services_path, notice: "Conta criada com sucesso!"
+    else
+      render :new, alert: "Erro ao criar conta!"
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :email, :phone, :password, :password_confirmation)
   end
 end
